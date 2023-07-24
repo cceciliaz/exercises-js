f1112cf85aca9051dae948dafdbe9839f30fe78c
cceciliaz <polianacecilia.656@gmail.com>
2023-07-24 11:09:50 -0300

    criado exercicio

---
+++ ORIGAMID/variaveis/aula1/exercicio.js
@@ -0,0 +1,12 @@
+var nome = "cecilia";
+var idade = 20;
+var comidaFavorita;
+var comidaFavorita = "pizza";
+
+var cor;
+var animal;
+var pais;
+var cidade;
+var sobreNome;
+
+    console.log(nome, idade, comidaFavorita);
---
+++ ORIGAMID/variaveis/aula1/script.js
@@ -0,0 +1,22 @@
+/* declarei a variavel a baixo e o valor e cecila, 28 */
+var nome = "cecilia";
+var idade = 20;
+
+/* essa funcao vai mostrar o console o valor da variavel no caso "cecilia", 28 */
+console.log(nome, idade);
+
+/* exemplo de declaracao de variaveis e multiplicacao */
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado + preco;
+
+/* ativando o console */
+console.log(totalPreco);
+
+/* exemplo de varias variaveis sem repetir a palavra var apenas usando virgula */
+var nome = "cecilia",
+    idade = 20
+    estado = "rn";
+    console.log(nome, idade, estado);
+
+/* se chamar uma variavel que nao existe ela vai dar o valor de "is not defined" dando um erro no console */
