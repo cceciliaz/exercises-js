3b56ee9f2ed17b0eabebba9eb414a256f781e906
cceciliaz <polianacecilia.656@gmail.com>
2023-07-24 12:21:06 -0300

    criado a pasta tipos de dados

---
+++ ORIGAMID/tiposdedados/aula2/exercicio2.js
@@ -0,0 +1,22 @@
+// declare uma variavel contendo uma string
+var nome = "cecilia";
+
+// declare uma variavel contendo um numero dentro de uma string
+var ano = "2020";
+
+// declare uma variavel com a sua idade
+var minhaIdade = 20;
+
+// declare duas variaveis, uma com seu nome e outra com seu sobrenome e some as mesmas
+var nome = "cecilia ",
+    sobreNome = " oliveira",
+    nomeCompleto = '${nome} ${sobrenome}';
+    console.log(nomeCompleto);
+
+// declare a seguinte frase em uma variavel: it´ time 
+var frase = "it´s time";
+console.log(frase);
+
+// verifique o tipo de variavel que tem no seu nome
+var nome = "cecilia";
+console.log(typeof nome);
---
+++ ORIGAMID/tiposdedados/aula2/script.js
@@ -0,0 +1,25 @@
+/*
+
+var nome = "cecilia"; // string
+var idade = 20; // numero
+var temfaculdade = true; // boolean
+var time; // undefined valor nao definido
+var comida = null; //
+var simolo = Symbol () // symcbol
+var novoObjeto = {} // objeto 
+
+define se a variavel foi numero ou string
+console.log(typeof idade);
+
+*/
+
+/* exemplo de concatenacao, quando uma string se junta a outra ou quando uma string se junta a um numero */
+var gols = 10;
+var frase = "jonas fez " + gols + " gols";
+console.log(frase);
+
+//exemplo de soma com numeros
+var paes = 10;
+var sucos = 5;
+var compraTotal = paes + sucos;
+console.log(compraTotal);
