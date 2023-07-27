6a6cfe5e33b0460567356535d64f1e29ae94c287
cceciliaz <polianacecilia.656@gmail.com>
2023-07-27 15:45:00 -0300

    crieado exercicio6.js

--- ORIGAMID/js-iniciante/objetos/exercicio6.js
+++ ORIGAMID/js-iniciante/objetos/exercicio6.js
@@ -1,1 +1,20 @@
-// crie um objeto com ps seus dados pessoas deve possuir pelo menos duas propriedades nome e sobrenome
+// crie um objeto com os seus dados pessoas deve possuir pelo menos duas propriedades nome e sobrenome:
+var dadosPessoas = {
+    nome:"cecilia",
+    sobreNome:"oliveira",
+    idade: 20,
+    estado: "rio grande do norte",
+}
+
+// crie um metodo no onjeto anterior que mostre o seu nome completo:
+dadosPessoas.nomeCompleto = function (){
+    return `${this.nome} ${this.sobreNome}`;
+}
+
+
+// modifique o valor da propriedade preco para 3000:
+var carro = {
+    preco: 1000,
+    portas: 4,
+    marca: "glaz",
+}
