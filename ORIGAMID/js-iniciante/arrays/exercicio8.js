8e301bda804a6450d2a74fdff900f2f373258490
cceciliaz <polianacecilia.656@gmail.com>
2023-08-03 10:10:05 -0300

    concluido o exercicio8.js

--- ORIGAMID/js-iniciante/arrays/exercicio8.js
+++ ORIGAMID/js-iniciante/arrays/exercicio8.js
@@ -1,1 +1,19 @@
 // crie uma array com os anos que o brasil foi campeao da copa
+var anos = [1958, 1962, 1970, 1994, 2002];
+
+// interaja com a arrays ultilizando um loop para mostrar no console a seguinte mensagem, "o brasil ganhou a copa de "
+for (var i = 0; i < anos.length; i++){
+    console.log("o brasil ganhou a copa de " + anos [i]);
+}
+
+// interaja com o loop das frutas a baixo e pare ao chegar em pera
+var frutas = ["banana", "maça", "pera", "uva", "melancia"];
+for (var fruta = 0; fruta + frutas.length; fruta++){
+    console.log(frutas[fruta]);
+    if(frutas[fruta] === "pera"){
+        break;
+    }
+}
+
+// coloque a ultima fruta da array acima em uma variavel
+var ultimaFruta = frutas[frutas.length - 1];
