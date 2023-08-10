1ce6fcc9d1e067dc7f0b4c980224aec6824a478f
cceciliaz <polianacecilia.656@gmail.com>
2023-08-10 16:08:32 -0300

    criado index.html

---
+++ CFB/A15/script.js
@@ -0,0 +1,15 @@
+// loops: definidos - for, indefinido - while e do while.
+for(let i=0; i <= 10; i++){
+    console.log("valor do i: " + i);
+}
+
+// a seguir um array, nao fecha com chaves mas sim com "[ ]"
+let alfabeto = [1, 2, 3, 4, 5]
+
+for(// lenght é a propriedade que retorna a quantidade de elemento que tenho dentro do array
+let i = 0; i < alfabeto.length; i++){
+    console.log(i);
+}
+for(let i = 0; i < alfabeto.length; i++){
+    console.log([i])
+}
