c88d16231412bd2f42548d9491fb4d9596d1b276
cceciliaz <polianacecilia.656@gmail.com>
2023-08-10 11:11:41 -0300

    criado as pastas sobre if else e switch case

---
+++ CFB/A13/script.js
@@ -0,0 +1,20 @@
+let numero = 5;
+
+if(numero > 10){
+    console.log("numeral maior que 10");
+} else if(numero > 5){
+    console.log("numeral esta entre 6 e 10");
+} else{
+    console.log("numeral esta entre 4 e 10");
+}
+console.log("fim do programa");
+
+// 
+
+let energia = 100,
+    clima = "chuva";
+if(energia > 70 && clima === "sol"){
+    console.log("vou a praia");
+} else{
+    console.log("vou ao cinema");
+}
---
+++ CFB/A14/script.js
@@ -0,0 +1,18 @@
+let pontos = 3;
+switch(pontos){
+    case 1:
+        console.log("promeiro lugar");
+        break
+    case 2:
+        console.log("segundo lugar");
+        break
+    case 3:
+        console.log("terceiro lugar");
+        break
+    case 10:
+        console.log("decimo lugar");
+        break
+    default:
+        console.log("nao subiu no podio");
+        break
+}
