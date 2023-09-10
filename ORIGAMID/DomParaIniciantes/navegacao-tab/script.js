3f66e1a01db447b690bb4639aa8616a18ed94894
cceciliaz <polianacecilia.656@gmail.com>
2023-09-10 19:50:42 -0300

    .

--- ORIGAMID/DomParaIniciantes/navegacao-tab/script.js
+++ ORIGAMID/DomParaIniciantes/navegacao-tab/script.js
@@ -1,20 +1,22 @@
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
-const tabConteudo = document.querySelectorAll(".js-tabconteudo section");
+function inittabnav (){
+    const tabMenu = document.querySelectorAll(".js-tabmenu li");
+    const tabConteudo = document.querySelectorAll(".js-tabconteudo section");
 
-if(tabMenu.length && tabConteudo,length) {
+    if(tabMenu.length && tabConteudo,length) {
+    tabConteudo[0].classList.add("ativo");
 
-tabConteudo[0].classList.add("ativo");
+    function activeTab(index){
+            tabConteudo.forEach((section) => {
+                section.classList.remove("ativo");
+            });
+            tabConteudo[index].classList.add("ativo");
+        }
 
-function activeTab(index){
-        tabConteudo.forEach((section) => {
-            section.classList.remove("ativo");
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener("click", () => {
+                activeTab(index);
+            });
         });
-        tabConteudo[index].classList.add("ativo");
     }
-
-    tabMenu.forEach((itemMenu, index) => {
-        itemMenu.addEventListener("click", () => {
-            activeTab(index);
-        });
-    });
 }
+inittabnav();
