f553b85868c5121278e13970dbc6c9b16fa0d6c3
“Cecilia <polianacecilia.656@gmail.com>
2024-02-01 19:24:54 -0300

    escopo lexico

---
+++ Js-LuizOtavio/funcoesAvancadas/4-escopolexico/index.js
@@ -0,0 +1,14 @@
+const nome = "ana"
+
+function falaNome(){
+    const nome = "luiz"
+    console.log(nome);
+}
+
+function usaFalaNome(){
+    falaNome();
+}
+
+usaFalaNome();
+
+// respeita o escopo 
