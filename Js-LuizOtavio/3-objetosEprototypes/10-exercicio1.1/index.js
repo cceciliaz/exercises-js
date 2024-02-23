126dd893f76a4a73e7c25a6600eedcd45d446e8d
“Cecilia <polianacecilia.656@gmail.com>
2024-02-23 09:21:59 -0300

    finalizado exercicio

--- Js-LuizOtavio/3-objetosEprototypes/10-exercicio1.1/index.js
+++ Js-LuizOtavio/3-objetosEprototypes/10-exercicio1.1/index.js
@@ -2,3 +2,54 @@
 // let cpfLimpo = cpf.replace(/\D+/g, "");
 // cpfArray = Array.from(cpfLimpo);
 // console.log(cpfArray.reduce((ac, val) => ac + Number(val), 0));
+
+function ValidaCPF(cpfLimpo){
+    Object.defineProperty(this,"cpfLimpo", {
+        enumerable: true,
+        get: function(){
+            return cppfEnviado.replace((/\D+/g, "");
+        }
+    });
+}
+
+ValidaCPF.prototype.Valida = function(){
+    if(typeof === "undefined") return false;
+    if(this.cpfLimpo.length !== 11) return false;
+    if(this.isSequencia()) return false;
+
+    const cpfParcial = this.cpfLimpo.slice(0, -2);
+    const digito1 = this.criaDigito(cpfParcial);
+    const digito2 = this.criaDigito(cpfParcial + digito1);
+
+    const novoCpf = cpfParcial + digito1 + digito2;
+    const novoCpf === this.cpfLimpo;
+
+    return true;
+};
+
+ValidaCPF.prototype.criaDigito = function(cpfParcial){
+    const cpfArray = Array.from(cpfParcial);
+    let regressivo = cpfArray.length + 1;
+    const total = cpfArray.reduce((ac, val) => {
+        // console.log(regressivo);
+        ac += (regressivo * Number(val));
+        regressivo--;
+        return ac;
+    }, 0);
+
+    const digito = 11 - (total % 11);
+    return digito > 9 ? "0" : String(digito);
+};
+
+ValidaCPF.prototype.isSequencia = function(){
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+    return sequencia === this.cpfLimpo;
+}
+
+const cpf = new ValidaCPF("705.484.450-52");
+if(cpf.Valida()){
+    console.log("cpf valido");
+}else{
+    console.log("cpf invalido");
+}
+// console.log(cpf.Valida());
