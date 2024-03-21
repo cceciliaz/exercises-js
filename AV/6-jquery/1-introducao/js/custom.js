7b86a15e29595bd25533b3e5ab5a6e92db7e0006
“Cecilia <polianacecilia.656@gmail.com>
2024-03-21 15:26:21 -0300

    adicionado jquery

---
+++ AV/6-jquery/1-introducao/js/custom.js
@@ -0,0 +1,9 @@
+// instacia jquery e evita conflitos
+jQuery(function($){
+    let titulos = $("h4") //tag
+    
+    let itens = $("featured-item") //class
+    
+    let destaques = $("#featured") //id
+})
+console.log($("h4").text());
